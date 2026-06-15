62417f940f38af5897674f164e19b960b2cb95da
Jesus-Pro13 <verde.terva.13@gmail.com>
2026-06-15 16:21:05 -0600

    Movimmiento y reacciones con las coliciones con los demas objetos implementados

--- juego.h
+++ juego.h
@@ -1,6 +1,7 @@
 #ifndef JUEGO_H
 #define JUEGO_H
 #include <stdio.h>
+#include <stdbool.h>
 #include <windows.h>
 #include <conio.h>
 #include "mapas.h"
@@ -61,61 +62,223 @@ void imprimirMapa(int pY, int pX, char mapa[ROWS][COLUMNS])
     }
 
     SetConsoleTextAttribute(hConsole, 7);
-}
-
-void juego()
+}void juego()
 {
     int playerY = 1;
     int playerX = 1;
     int puntaje = 0;
-    int llave = 0;
     int monedasRecogidas = 0;
     int modedastotales = 0;
-    //bool haveKey = false;
-    fflush(stdin);
+    int pasos = 0;
+    bool haveKey = false;
+    int nivel = 1;
     char tecla = 'm';
-    
-     modedastotales = monedasTotales(&mapa1[0][0], ROWS * COLUMNS, 'M');
-     int libres = contarCeldaslibres(&mapa1[0][0], ROWS * COLUMNS);
-   
+
+    char (*mapaActual)[COLUMNS] = mapa1; 
+
+    modedastotales = monedasTotales(&mapaActual[0][0], ROWS * COLUMNS, 'M');
+    int libres = contarCeldaslibres(&mapaActual[0][0], ROWS * COLUMNS);
+
     while (tecla != 113 && tecla != EOF)
     {
         system("cls");
-        printf("--- NIVEL 1 ---\n");
+        printf("--- NIVEL %d ---\n", nivel);
         printf("Puntaje: %d\n", puntaje);
         printf("Monedas recogidas: %d\n", monedasRecogidas);
         printf("Monedas totales: %d\n", modedastotales);
-         printf("Celdas libres: %d\n", libres);
-        imprimirMapa(playerY, playerX, mapa1);
+        printf("Celdas libres: %d\n", libres);
+        
+        imprimirMapa(playerY, playerX, mapaActual);
 
         tecla = _getch();
 
+    
         if (tecla == 'w' || tecla == 'W')
         {
-            if (validar_movimiento(&mapa1[0][0], COLUMNS, (playerY - 1), playerX, '#'))
+          
+            if (!findObject(&mapaActual[0][0], COLUMNS, (playerY - 1), playerX, '#'))
             {
+                if(findObject(&mapaActual[0][0], COLUMNS, (playerY - 1), playerX, 'M')) {
+                    monedasRecogidas++;
+                    mapaActual[playerY - 1][playerX] = '.';
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, (playerY - 1), playerX, 'K')) {
+                    haveKey = true;
+                    mapaActual[playerY - 1][playerX] = '.';
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, (playerY - 1), playerX, 'D')) {
+                    if(haveKey) {
+                        mapaActual[playerY - 1][playerX] = '.'; 
+                    } else {
+                        printf("Necesitas la llave para abrir la puerta.\n");
+                        Sleep(1000);
+                        continue; 
+                    }
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, (playerY - 1), playerX, 'E')) {
+                    printf("¡Has ganado el nivel %d!\n", nivel);
+                    Sleep(2000);
+                    nivel++;
+                    
+                    if(nivel == 2) {
+                        mapaActual = mapa2;
+                    } else if(nivel == 3) {
+                        mapaActual = mapa3;
+                    } else {
+                        printf("¡Felicidades, has completado el juego!\n");
+                        Sleep(3000);
+                        return;
+                    }
+                    
+                    playerY = 1;
+                    playerX = 1;
+                    haveKey = false;
+                    modedastotales = monedasTotales(&mapaActual[0][0], ROWS * COLUMNS, 'M');
+                    libres = contarCeldaslibres(&mapaActual[0][0], ROWS * COLUMNS);
+                    continue; 
+                } 
+                
                 playerY--;
+                pasos++;
             }
         }
+
         if (tecla == 's' || tecla == 'S')
         {
-            if (validar_movimiento(&mapa1[0][0], COLUMNS, (playerY + 1), playerX, '#'))
+            if (!findObject(&mapaActual[0][0], COLUMNS, (playerY + 1), playerX, '#'))
             {
+                if(findObject(&mapaActual[0][0], COLUMNS, (playerY + 1), playerX, 'M')) {
+                    monedasRecogidas++;
+                    mapaActual[playerY + 1][playerX] = '.';
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, (playerY + 1), playerX, 'K')) {
+                    haveKey = true;
+                    mapaActual[playerY + 1][playerX] = '.';
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, (playerY + 1), playerX, 'D')) {
+                    if(haveKey) {
+                        mapaActual[playerY + 1][playerX] = '.';
+                    } else {
+                        printf("Necesitas la llave para abrir la puerta.\n");
+                        Sleep(1000);
+                        continue;
+                    }
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, (playerY + 1), playerX, 'E')) {
+                    printf("¡Has ganado el nivel %d!\n", nivel);
+                    Sleep(2000);
+                    nivel++;
+                    if(nivel == 2) {
+                        mapaActual = mapa2;
+                    } else if(nivel == 3) {
+                        mapaActual = mapa3;
+                    } else {
+                        printf("¡Felicidades, has completado el juego!\n");
+                        Sleep(3000);
+                        return;
+                    }
+                    playerY = 1;
+                    playerX = 1;
+                    haveKey = false;
+                    modedastotales = monedasTotales(&mapaActual[0][0], ROWS * COLUMNS, 'M');
+                    libres = contarCeldaslibres(&mapaActual[0][0], ROWS * COLUMNS);
+                    continue;
+                }
+                
                 playerY++;
+                pasos++;
             }
         }
         if (tecla == 'a' || tecla == 'A')
         {
-            if (validar_movimiento(&mapa1[0][0], COLUMNS, playerY, (playerX - 1), '#'))
+            if (!findObject(&mapaActual[0][0], COLUMNS, playerY, (playerX - 1), '#'))
             {
+                if(findObject(&mapaActual[0][0], COLUMNS, playerY, (playerX - 1), 'M')) {
+                    monedasRecogidas++;
+                    mapaActual[playerY][playerX - 1] = '.';
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, playerY, (playerX - 1), 'K')) {
+                    haveKey = true;
+                    mapaActual[playerY][playerX - 1] = '.';
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, playerY, (playerX - 1), 'D')) {
+                    if(haveKey) {
+                        mapaActual[playerY][playerX - 1] = '.';
+                    } else {
+                        printf("Necesitas la llave para abrir la puerta.\n");
+                        Sleep(1000);
+                        continue;
+                    }
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, playerY, (playerX - 1), 'E')) {
+                    printf("¡Has ganado el nivel %d!\n", nivel);
+                    Sleep(2000);
+                    nivel++;
+                    if(nivel == 2) {
+                        mapaActual = mapa2;
+                    } else if(nivel == 3) {
+                        mapaActual = mapa3;
+                    } else {
+                        printf("¡Felicidades, has completado el juego!\n");
+                        Sleep(3000);
+                        return;
+                    }
+                    playerY = 1;
+                    playerX = 1;
+                    haveKey = false;
+                    modedastotales = monedasTotales(&mapaActual[0][0], ROWS * COLUMNS, 'M');
+                    libres = contarCeldaslibres(&mapaActual[0][0], ROWS * COLUMNS);
+                    continue;
+                }
+                
                 playerX--;
+                pasos++;
             }
         }
         if (tecla == 'd' || tecla == 'D')
         {
-            if (validar_movimiento(&mapa1[0][0], COLUMNS, playerY, (playerX + 1), '#'))
+            if (!findObject(&mapaActual[0][0], COLUMNS, playerY, (playerX + 1), '#'))
             {
+                if(findObject(&mapaActual[0][0], COLUMNS, playerY, (playerX + 1), 'M')) {
+                    monedasRecogidas++;
+                    mapaActual[playerY][playerX + 1] = '.';
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, playerY, (playerX + 1), 'K')) {
+                    haveKey = true;
+                    mapaActual[playerY][playerX + 1] = '.';
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, playerY, (playerX + 1), 'D')) {
+                    if(haveKey) {
+                        mapaActual[playerY][playerX + 1] = '.';
+                    } else {
+                        printf("Necesitas la llave para abrir la puerta.\n");
+                        Sleep(1000);
+                        continue;
+                    }
+                }
+                if(findObject(&mapaActual[0][0], COLUMNS, playerY, (playerX + 1), 'E')) {
+                    printf("¡Has ganado el nivel %d!\n", nivel);
+                    Sleep(2000);
+                    nivel++;
+                    if(nivel == 2) {
+                        mapaActual = mapa2;
+                    } else if(nivel == 3) {
+                        mapaActual = mapa3;
+                    } else {
+                        printf("¡Felicidades, has completado el juego!\n");
+                        Sleep(3000);
+                        return;
+                    }
+                    playerY = 1;
+                    playerX = 1;
+                    haveKey = false;
+                    modedastotales = monedasTotales(&mapaActual[0][0], ROWS * COLUMNS, 'M');
+                    libres = contarCeldaslibres(&mapaActual[0][0], ROWS * COLUMNS);
+                    continue;
+                }
+                
                 playerX++;
+                pasos++;
             }
         }
     }
